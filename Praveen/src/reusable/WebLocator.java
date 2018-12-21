d03c8e6752a54b9ee8ad89c87f98e153ffed3ea4
Praveen Reddy <gangulapraveenreddy@gmail.com>
2018-12-21 22:34:13 +0530

    Reuasable facebook

---
+++ Praveen/src/reusable/WebLocator.java
@@ -0,0 +1,9 @@
+package reusable;
+
+public class WebLocator
+{
+public static String Email="//input[@id='email']";
+public static String password="//input[@id='pass']";
+public static String login="//input[@value='Log In']";
+}
+
